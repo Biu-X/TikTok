e7d4428bb621ac3e7caf5ab344f49aa3b68139c5
hiifong <i@hiif.ong>
2023-08-03 21:00:51 +0800

    fix config logic

--- modules/config/config.go
+++ modules/config/config.go
@@ -15,50 +15,48 @@ func Init() {
 	config.SetConfigName("tiktok")
 	config.AddConfigPath("./conf/")
 	config.AddConfigPath("./")
-	config.AddConfigPath("../../conf")
 	config.AddConfigPath("$HOME/.tiktok/")
 	config.AddConfigPath("/etc/tiktok/")
 	config.SetConfigType("yml")
-
-	config.SetDefault("server", map[string]interface{}{
-		"port": 8080,
-		"mode": "prod",
-	})
-
-	config.SetDefault("log", map[string]interface{}{
-		"level": "debug",
-		"mode":  []string{"console", "file"},
-		"path":  "./log",
-	})
-
-	config.SetDefault("mysql", map[string]interface{}{
-		"host":     "127.0.0.1",
-		"port":     3306,
-		"username": "root",
-		"password": "123456",
-		"database": "demo",
-	})
-
-	config.SetDefault("redis", map[string]interface{}{
-		"host":     "127.0.0.1",
-		"port":     6379,
-		"password": "123456",
-		"database": 0,
-	})
-
-	if err := config.ReadInConfig(); err != nil {
-		var configFileNotFoundError viper.ConfigFileNotFoundError
-		if errors.As(err, &configFileNotFoundError) {
-			// 配置文件未找到错误
-			fmt.Println("Config file not found!")
-		}
-	}
 
 	config.WatchConfig()
 	config.OnConfigChange(func(e fsnotify.Event) {
 		// 配置文件发生变更之后会调用的回调函数
 		fmt.Println("Config file changed:", e.Name)
 	})
+
+	if err := config.ReadInConfig(); err != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(err, &configFileNotFoundError) {
+			// 配置文件未找到错误
+			fmt.Println("config file not found use default config")
+			config.SetDefault("server", map[string]interface{}{
+				"port": 8080,
+				"mode": "prod",
+			})
+
+			config.SetDefault("log", map[string]interface{}{
+				"level": "debug",
+				"mode":  []string{"console", "file"},
+				"path":  "./log",
+			})
+
+			config.SetDefault("mysql", map[string]interface{}{
+				"host":     "127.0.0.1",
+				"port":     3306,
+				"username": "root",
+				"password": "123456",
+				"database": "demo",
+			})
+
+			config.SetDefault("redis", map[string]interface{}{
+				"host":     "127.0.0.1",
+				"port":     6379,
+				"password": "123456",
+				"database": 0,
+			})
+		}
+	}
 }
 
 func Get(key string) interface{} {
