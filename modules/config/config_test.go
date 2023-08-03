594bb7b9dcb69d078a40866c099c51af994fa8b6
hiifong <i@hiif.ong>
2023-08-03 20:50:05 +0800

    fix config test (#13)

--- modules/config/config_test.go
+++ modules/config/config_test.go
@@ -1,1 +1,12 @@
-package config
+package config_test
+
+import (
+	"biu-x.org/TikTok/modules/config"
+	"fmt"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	config.Init()
+	fmt.Println(config.GetString("mysql.host"))
+}
