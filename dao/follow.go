a99eec0f57135ed7be045a53d8cadd04a6beb6e9
Zaire <3147879462@qq.com>
2023-08-07 15:31:38 +0800

    feat FollowDAO

--- dao/follow.go
+++ dao/follow.go
@@ -1,1 +1,30 @@
 package dao
+
+import (
+	"biu-x.org/TikTok/dal/query"
+	"biu-x.org/TikTok/model"
+)
+
+func CreateFollow(follow *model.Follow) (err error) {
+	f := query.Follow
+	err = f.Create(follow)
+	return err
+}
+
+func GetFollowByID(id int64) (follow *model.Follow, err error) {
+	f := query.Follow
+	follow, err = f.Where(f.ID.Eq(id)).First()
+	return follow, err
+}
+
+func GetFollowByBoth(userID int64, followID int64) (follow *model.Follow, err error) {
+	f := query.Follow
+	follow, err = f.Where(f.UserID.Eq(userID), f.FollowerID.Eq(followID)).First()
+	return follow, err
+}
+
+func SetFollowCancelByID(id int64, cancel bool) (err error) {
+	f := query.Follow
+	_, err = f.Where(f.ID.Eq(id)).Update(f.Cancel, cancel)
+	return err
+}
