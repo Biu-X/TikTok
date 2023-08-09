ec5ed495345a9287bd10cc8a9b217c5f8f5761fc
blockChain <33158355+1055373165@users.noreply.github.com>
2023-08-09 20:47:56 +0800

    Smy (#49)
    - User Signup And Login 
    - JWT Generate And Verify
    
    ---------
    
    Co-authored-by: hiifong <i@hiif.ong>

--- module/middleware/jwt/jwt_test.go
+++ module/middleware/jwt/jwt_test.go
@@ -1,10 +1,37 @@
-package jwt
+package jwt_test
 
 import (
+	"fmt"
 	"testing"
+
+	"biu-x.org/TikTok/dal/query"
+	"biu-x.org/TikTok/model"
+	"biu-x.org/TikTok/module/config"
+	"biu-x.org/TikTok/module/db"
+	"biu-x.org/TikTok/module/middleware/jwt"
 )
 
-func TestSingToken(t *testing.T) {
-	token := CreateToken()
-	ParsToken(token)
+func TestSingToken1(t *testing.T) {
+	// 配置初始化
+	config.Init()
+	db.Init()
+
+	// 模拟注册（新建一个用户）
+	u := query.User
+	user := &model.User{Name: "TableNewUser", Password: "abc", Signature: "newtess", Avatar: "avatar", BackgroundImage: "background"}
+	err := u.Create(user)
+	if err != nil {
+		t.Fatalf("create user failed, err: %v", err)
+	}
+
+	// 模拟登录（生成 JWT token）
+	token := jwt.GenerateToken("TableNewUser")
+	fmt.Printf("token: %#v\n", token)
+
+	useClaims, err := jwt.ParseToken(token)
+	if err != nil {
+		t.Fatalf("jwt token generate success, but vaild failed....")
+	}
+
+	fmt.Println("token 解析成功，获取到的用户信息：", *useClaims)
 }
