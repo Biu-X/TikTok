7c210ee28736e17fe6d1051e9671a4fe4c5282dd
1055373165 <1055373165@qq.com>
2023-08-18 17:45:07 +0800

    newest upate

--- TikTok.go
+++ TikTok.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-	"biu-x.org/TikTok/module/config"
-	"biu-x.org/TikTok/module/db"
-	"biu-x.org/TikTok/module/log"
-	"biu-x.org/TikTok/service/user"
-	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+
+	"biu-x.org/TikTok/cmd"
 )
 
 const version = "v0.1"
 
 func main() {
-	config.Init()
-	log.Init()
-	db.Init()
-	r := gin.Default()
-	r.POST("/signup", user.Signup)
-	r.POST("/login", user.Login)
-	r.GET("/userinfo", user.UserInfo)
-	r.Run(":8080")
-	// app := cmd.NewApp()
-	// app.Name = "TikTok"
-	// app.Usage = "TikTok Server"
-	// app.Description = "A TikTok Server Written in Go"
-	// app.Version = version
+	app := cmd.NewApp()
+	app.Name = "TikTok"
+	app.Usage = "TikTok Server"
+	app.Description = "A TikTok Server Written in Go"
+	app.Version = version
 
-	// err := app.Run(os.Args)
-	// if err != nil {
-	// 	log.Printf("Failed to run with %s: %v\\n", os.Args, err)
-	// }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Printf("Failed to run with %s: %v\\n", os.Args, err)
+	}
 }
