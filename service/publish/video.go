3ee5136930c72fd87e337265624bd2db8e271ca9
hiifong <i@hiif.ong>
2023-08-19 21:46:29 +0800

    fix

--- service/publish/video.go
+++ service/publish/video.go
@@ -13,24 +13,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"os"
 	"strconv"
-	"sync"
 )
 
 // Action 投稿操作 /douyin/publish/action/
 func Action(c *gin.Context) {
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
 	// 在完成上传视频后把临时文件都删除
-	defer func(wg *sync.WaitGroup) {
-		wg.Wait()
+	defer func() {
 		path := c.GetString("user_id")
 		err := os.RemoveAll(path)
 		if err != nil {
 			log.Logger.Error(err)
 			return
 		}
-	}(&wg)
+	}()
 
 	// 获取视频
 	file, err := c.FormFile("data")
@@ -68,11 +63,15 @@ func Action(c *gin.Context) {
 	image, err := ffmpeg.GetCoverFromVideo(fileName, 10)
 	if err != nil {
 		log.Logger.Error(err)
+		response.ErrRespWithMsg(c, err.Error())
+		return
 	}
 
 	img, err := imaging.Decode(image)
 	if err != nil {
 		log.Logger.Error(err)
+		response.ErrRespWithMsg(c, err.Error())
+		return
 	}
 
 	cover := fmt.Sprintf("%v/%v-cover.jpeg", aid, file.Filename)
@@ -81,6 +80,8 @@ func Action(c *gin.Context) {
 	err = imaging.Save(img, cover)
 	if err != nil {
 		log.Logger.Error(err)
+		response.ErrRespWithMsg(c, err.Error())
+		return
 	}
 
 	// 上传视频到对象存储
@@ -95,6 +96,8 @@ func Action(c *gin.Context) {
 	err = s3.PutFromFile(cover, cover)
 	if err != nil {
 		log.Logger.Error(err)
+		response.ErrRespWithMsg(c, err.Error())
+		return
 	}
 
 	err = dao.CreateVideo(&model.Video{
@@ -104,9 +107,9 @@ func Action(c *gin.Context) {
 		Title:    c.PostForm("title"),
 	})
 	if err != nil {
+		log.Logger.Error(err)
 		response.ErrResp(c)
 		return
 	}
 	response.OKResp(c)
-	wg.Done()
 }
