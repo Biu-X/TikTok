c7c553809f5099967aba3c8535fa477ce18db10b
1055373165 <1055373165@qq.com>
2023-08-20 15:47:57 +0800

    fix auth logic

--- TikTok.go
+++ TikTok.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"biu-x.org/TikTok/cmd"
+	"biu-x.org/TikTok/module/config"
+	"biu-x.org/TikTok/module/db"
+	"biu-x.org/TikTok/module/log"
+	"biu-x.org/TikTok/router"
 )
 
 const version = "v0.1"
 
 func main() {
-	app := cmd.NewApp()
-	app.Name = "TikTok"
-	app.Usage = "TikTok Server"
-	app.Description = "A TikTok Server Written in Go"
-	app.Version = version
+	config.Init()
+	log.Init()
+	db.Init()
+	router.Init()
+	// app := cmd.NewApp()
+	// app.Name = "TikTok"
+	// app.Usage = "TikTok Server"
+	// app.Description = "A TikTok Server Written in Go"
+	// app.Version = version
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Printf("Failed to run with %s: %v\\n", os.Args, err)
-	}
+	// err := app.Run(os.Args)
+	// if err != nil {
+	// 	log.Printf("Failed to run with %s: %v\\n", os.Args, err)
+	// }
 }
