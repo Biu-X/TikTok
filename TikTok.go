909e5db54d6644fc73b0031304b0bd713c2fe075
1055373165 <1055373165@qq.com>
2023-08-20 15:49:11 +0800

    main function

--- TikTok.go
+++ TikTok.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"biu-x.org/TikTok/module/config"
-	"biu-x.org/TikTok/module/db"
-	"biu-x.org/TikTok/module/log"
-	"biu-x.org/TikTok/router"
+	"os"
+
+	"log"
+
+	"biu-x.org/TikTok/cmd"
 )
 
 const version = "v0.1"
 
 func main() {
-	config.Init()
-	log.Init()
-	db.Init()
-	router.Init()
-	// app := cmd.NewApp()
-	// app.Name = "TikTok"
-	// app.Usage = "TikTok Server"
-	// app.Description = "A TikTok Server Written in Go"
-	// app.Version = version
+	app := cmd.NewApp()
+	app.Name = "TikTok"
+	app.Usage = "TikTok Server"
+	app.Description = "A TikTok Server Written in Go"
+	app.Version = version
 
-	// err := app.Run(os.Args)
-	// if err != nil {
-	// 	log.Printf("Failed to run with %s: %v\\n", os.Args, err)
-	// }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Printf("Failed to run with %s: %v\\n", os.Args, err)
+	}
 }
