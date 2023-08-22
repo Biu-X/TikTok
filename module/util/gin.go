f8a15954511bb94dd4c3261aed0606a6b143165b
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-08-22 10:24:24 +0800

    fix favorite cancel & refactor

--- module/util/gin.go
+++ module/util/gin.go
@@ -8,12 +8,6 @@ import (
 
 // GetUserIDFromGinContext 从 RequireAuth 处读取 user_id
 func GetUserIDFromGinContext(c *gin.Context) int64 {
-	// 判断是否登陆
-	if _, ok := c.Get("is_login"); !ok {
-		log.Logger.Infof("current login status: %v", ok)
-		return 0
-	}
-
 	// 已登录
 	userID, err := strconv.ParseInt(c.GetString("user_id"), 10, 64)
 	if err != nil {
--- service/relation/action.go
+++ service/relation/action.go
@@ -13,19 +13,19 @@ import (
 func Action(c *gin.Context) {
 	userID := util.GetUserIDFromGinContext(c)
 	// 从 request 中查询
-	toUserId, _ := strconv.ParseInt(c.Query("to_user_id"), 10, 64)    // 对方用户id
+	toUserID, _ := strconv.ParseInt(c.Query("to_user_id"), 10, 64)    // 对方用户id
 	actionType, _ := strconv.ParseInt(c.Query("action_type"), 10, 64) // 1-关注，2-取消关注
 
 	// 根据 action_type 执行不同的操作
 	if actionType == 1 {
-		err := dao.SetFollowingByBoth(toUserId, userID)
+		err := dao.SetFollowingByBoth(toUserID, userID)
 		if err != nil {
 			log.Logger.Error(err.Error())
 			response.ErrRespWithMsg(c, err.Error())
 			return
 		}
 	} else {
-		err := dao.SetFollowCancelByBoth(toUserId, userID)
+		err := dao.SetFollowCancelByBoth(toUserID, userID)
 		if err != nil {
 			log.Logger.Error(err.Error())
 			response.ErrRespWithMsg(c, err.Error())
