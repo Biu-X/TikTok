a27bae42ab33dc2542c1a7d6fa99ba31fd0f08ae
hiifong <i@hiif.ong>
2023-08-22 17:08:14 +0800

    add redis

--- module/cache/redis.go
+++ module/cache/redis.go
@@ -2,36 +2,45 @@ package cache
 
 import (
 	"biu-x.org/TikTok/module/config"
-	"biu-x.org/TikTok/module/log"
 	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
-	"strconv"
 	"time"
 )
 
 type Client struct {
-	*redis.Client //nolint:typecheck
-	ctx           context.Context
+	C   *redis.Client //nolint:typecheck
+	ctx context.Context
 }
 
-func NewRedisClient() *Client {
-	db, _ := strconv.Atoi(fmt.Sprintf("%v", config.Get("redis.db")))
+func NewRedisClients(clients map[string]*Client) {
+	ctx := context.Background()
+	n := 0
+	for k, _ := range clients {
+		r := redis.NewClient(&redis.Options{ //nolint:typecheck
+			Addr:     fmt.Sprintf("%v:%v", config.Get("redis.host"), config.Get("redis.port")),
+			Password: fmt.Sprintf("%v", config.Get("redis.password")),
+			DB:       n + 1,
+		})
+		n++
+		clients[k] = &Client{C: r, ctx: ctx}
+	}
+}
+
+func NewRedisClient(n int) *Client {
+	ctx := context.Background()
 	r := redis.NewClient(&redis.Options{ //nolint:typecheck
 		Addr:     fmt.Sprintf("%v:%v", config.Get("redis.host"), config.Get("redis.port")),
 		Password: fmt.Sprintf("%v", config.Get("redis.password")),
-		DB:       db,
+		DB:       n,
 	})
-	ctx := context.Background()
-	return &Client{Client: r, ctx: ctx}
+	return &Client{C: r, ctx: ctx}
 }
 
 func (c Client) Set(key string, value interface{}) *redis.StatusCmd { //nolint:typecheck
-	log.Logger.Debugf("redis set key: %v, value: %v", key, value)
-	return c.Client.Set(c.ctx, key, value, 10*time.Hour)
+	return c.C.Set(c.ctx, key, value, 10*time.Hour)
 }
 
 func (c Client) Get(key interface{}) *redis.StringCmd { //nolint:typecheck
-	log.Logger.Debugf("redis get key: %v", key)
-	return c.Client.Get(c.ctx, key.(string))
+	return c.C.Get(c.ctx, key.(string))
 }
--- module/cache/redis_test.go
+++ module/cache/redis_test.go
@@ -1,1 +1,32 @@
 package cache
+
+import (
+	"biu-x.org/TikTok/module/config"
+	"biu-x.org/TikTok/module/db"
+	"biu-x.org/TikTok/module/log"
+	"biu-x.org/TikTok/module/oss"
+	"testing"
+)
+
+func TestNewRedisClient(t *testing.T) {
+	config.Init()
+	log.Init()
+	db.Init()
+	oss.Init()
+
+	clients := map[string]*Client{
+		"IPLimit": &Client{},
+		"Feed":    &Client{},
+	}
+	NewRedisClients(clients)
+
+	for i, c := range clients {
+		log.Logger.Infof("client %v: %v", i, c)
+	}
+	ipLimit := clients["IPLimit"]
+	ipLimit.Set("name", "lala")
+	name := ipLimit.Get("name")
+	log.Logger.Infof("%v", name.Val())
+	name = ipLimit.C.Get(ipLimit.ctx, "name")
+	log.Logger.Infof("%v", name.Val())
+}
