32ea2d6379df2ca2f87b92ce41f5af97f393b6bf
离歌 <65994850+Tohrusky@users.noreply.github.com>
2023-08-22 19:38:06 +0800

    fix friend list (#120)

--- service/relation/friend_list.go
+++ service/relation/friend_list.go
@@ -1,14 +1,11 @@
 package relation
 
 import (
-	"errors"
-
 	"biu-x.org/TikTok/dao"
 	"biu-x.org/TikTok/module/log"
 	"biu-x.org/TikTok/module/response"
 	"biu-x.org/TikTok/module/util"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 // FriendList /douyin/relation/friend/list/ - 用户好友列表
@@ -26,20 +23,15 @@ func FriendList(c *gin.Context) {
 
 	for _, followerID := range followerIDs {
 		userRes, err := response.GetUserResponseByID(followerID, ownerID)
-
 		if err != nil {
 			log.Logger.Error(err)
 			continue
 		}
 
 		message, err := dao.GetLatestBidirectionalMessage(ownerID, followerID)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			continue
-		}
-		// 后端数据库错误，直接返回
 		if err != nil {
+			// 第一次加好友时，没有消息可以获取，这里忽略错误
 			log.Logger.Error(err)
-			return
 		}
 
 		var msgType int64
