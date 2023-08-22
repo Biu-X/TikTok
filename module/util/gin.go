2349260ebe02286b3c0dbfab401237828878e9ab
1055373165 <1055373165@qq.com>
2023-08-22 15:27:17 +0800

    change gin.go

--- module/util/gin.go
+++ module/util/gin.go
@@ -1,15 +1,21 @@
 package util
 
 import (
+	"strconv"
+
 	"biu-x.org/TikTok/module/log"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 // GetUserIDFromGinContext 从 RequireAuth 处读取 user_id
 func GetUserIDFromGinContext(c *gin.Context) int64 {
+	userId := c.GetString("user_id")
+	// 未登录
+	if len(userId) == 0 {
+		return 0
+	}
 	// 已登录
-	userID, err := strconv.ParseInt(c.GetString("user_id"), 10, 64)
+	userID, err := strconv.ParseInt(userId, 10, 64)
 	if err != nil {
 		log.Logger.Errorf("strconv.ParseInt failed, err: %v", err)
 		return 0
