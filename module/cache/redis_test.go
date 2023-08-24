82cf5fb618fe44e3562097c255838a93eebc5d26
hiifong <i@hiif.ong>
2023-08-24 22:03:59 +0800

    test

--- module/cache/redis_test.go
+++ module/cache/redis_test.go
@@ -1,31 +1,8 @@
 package cache
 
 import (
-	"github.com/Biu-X/TikTok/module/config"
-	"github.com/Biu-X/TikTok/module/db"
-	"github.com/Biu-X/TikTok/module/log"
-	"github.com/Biu-X/TikTok/module/oss"
 	"testing"
 )
 
 func TestNewRedisClient(t *testing.T) {
-	defer func(c map[RDB]*Client) {
-		for k, _ := range c {
-			err := c[k].C.Close()
-			if err != nil {
-				return
-			}
-		}
-	}(Clients)
-	config.Init()
-	log.Init()
-	db.Init()
-	oss.Init()
-	Init()
-	feed := Clients[Feed]
-	result, err := feed.Ping().Result()
-	if err != nil {
-		return
-	}
-	log.Logger.Infof("Ping -> %v", result)
 }
--- module/db/db.go
+++ module/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/Biu-X/TikTok/dal/model"
 	"github.com/Biu-X/TikTok/dal/query"
 	"github.com/Biu-X/TikTok/module/config"
 	"github.com/Biu-X/TikTok/module/log"
@@ -12,6 +13,18 @@ var DB *gorm.DB
 
 func Init() {
 	DB = ConnectDB(config.MySQLDSN())
+	err := DB.AutoMigrate(
+		model.Comment{},
+		model.Favorite{},
+		model.Follow{},
+		model.Message{},
+		model.User{},
+		model.Video{},
+	)
+	if err != nil {
+		log.Logger.Error(err)
+		return
+	}
 	query.SetDefault(DB)
 	log.Logger.Debugf("Set query default database")
 }
