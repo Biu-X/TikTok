fae618358e19ed68325544844d6d35347547e43c
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-08-24 21:28:13 +0800

    update CI-test

--- module/util/random_test.go
+++ module/util/random_test.go
@@ -1,13 +1,18 @@
 package util
 
-//func TestGetRandomString(t *testing.T) {
-//	a := assert.New(t)
-//	a.Equal(10, len(GetRandomString(10)))
-//	a.Equal(20, len(GetRandomString(20)))
-//	// 生成100长度字符串，测试是否有重复
-//	for i := 0; i < 100; i++ {
-//		for j := i + 1; j < 100; j++ {
-//			a.NotEqual(GetRandomString(100), GetRandomString(100))
-//		}
-//	}
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetRandomString(t *testing.T) {
+	a := assert.New(t)
+	a.Equal(10, len(GetRandomString(10)))
+	a.Equal(20, len(GetRandomString(20)))
+	// 生成100长度字符串，测试是否有重复
+	for i := 0; i < 100; i++ {
+		for j := i + 1; j < 100; j++ {
+			a.NotEqual(GetRandomString(100), GetRandomString(100))
+		}
+	}
+}
